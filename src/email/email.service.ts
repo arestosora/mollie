2d2860ac3c8c0d49586886c14a0802efe7a50c01
Ares <89419093+arestosora@users.noreply.github.com>
2024-07-17 22:30:47 -0500

    chore: improved email design

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -15,10 +15,10 @@ export class EmailService {
         },
     });
 
-    private async sendEmail(to: string, subject: string, html: string): Promise<void> {
+    private async sendEmailToRecipient(to: string, subject: string, html: string): Promise<void> {
         const mailOptions = {
             from: process.env.EMAIL_SENDER,
-            to: `${to}, ${process.env.EMAIL_RECEIVER}`,
+            to: to,
             subject: subject,
             html: html,
         };
@@ -27,42 +27,225 @@ export class EmailService {
         this.logger.log(`Email sent with subject: ${subject} to: ${to}`);
     }
 
-    private generateHtml(data: object): string {
-        let html = '<h1>Information</h1><table>';
+    private async sendEmailToReceiver(subject: string, html: string): Promise<void> {
+        const mailOptions = {
+            from: process.env.EMAIL_SENDER,
+            to: process.env.EMAIL_RECEIVER,
+            subject: subject,
+            html: html,
+        };
+
+        await this.transporter.sendMail(mailOptions);
+        this.logger.log(`Email sent with subject: ${subject} to receiver`);
+    }
+
+    private generateHtmlForRecipient(data: object, subject: string): string {
+        let html = `
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Information</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 20px;
+                }
+                .container {
+                    background-color: #fff;
+                    margin: 0 auto;
+                    padding: 20px;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    max-width: 600px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin: 20px 0;
+                }
+                th, td {
+                    padding: 12px;
+                    border-bottom: 1px solid #ddd;
+                }
+                th {
+                    background-color: #f4f4f4;
+                    text-align: left;
+                }
+                td {
+                    background-color: #fafafa;
+                }
+                tr:hover {
+                    background-color: #f1f1f1;
+                }
+                .btn {
+                    display: inline-block;
+                    margin-top: 20px;
+                    padding: 10px 20px;
+                    color: #fff;
+                    background-color: #007BFF;
+                    border: none;
+                    border-radius: 5px;
+                    text-align: center;
+                    text-decoration: none;
+                    font-size: 16px;
+                }
+                .btn:hover {
+                    background-color: #0056b3;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>You've ordered successfully your ${subject} certificate with the information below:</h1>
+                <table>
+                    <tr>
+                        <th>Clave</th>
+                        <th>Valor</th>
+                    </tr>`;
+                    
         for (const [key, value] of Object.entries(data)) {
             html += `<tr><td><strong>${key}:</strong></td><td>${value}</td></tr>`;
         }
-        html += '</table>';
+
+        html += `
+                </table>
+            </div>
+        </body>
+        </html>`;
+        
+        return html;
+    }
+
+    private generateHtmlForReceiver(data: object): string {
+        let html = `
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Information</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 20px;
+                }
+                .container {
+                    background-color: #fff;
+                    margin: 0 auto;
+                    padding: 20px;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    max-width: 600px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin: 20px 0;
+                }
+                th, td {
+                    padding: 12px;
+                    border-bottom: 1px solid #ddd;
+                }
+                th {
+                    background-color: #f4f4f4;
+                    text-align: left;
+                }
+                td {
+                    background-color: #fafafa;
+                }
+                tr:hover {
+                    background-color: #f1f1f1;
+                }
+                .btn {
+                    display: inline-block;
+                    margin-top: 20px;
+                    padding: 10px 20px;
+                    color: #fff;
+                    background-color: #007BFF;
+                    border: none;
+                    border-radius: 5px;
+                    text-align: center;
+                    text-decoration: none;
+                    font-size: 16px;
+                }
+                .btn:hover {
+                    background-color: #0056b3;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>Información</h1>
+                <table>
+                    <tr>
+                        <th>Clave</th>
+                        <th>Valor</th>
+                    </tr>`;
+                    
+        for (const [key, value] of Object.entries(data)) {
+            html += `<tr><td><strong>${key}:</strong></td><td>${value}</td></tr>`;
+        }
+
+        html += `
+                </table>
+            </div>
+        </body>
+        </html>`;
+        
         return html;
     }
 
     async createUserInfo(email: string, data: CreateUserInfoDto): Promise<void> {
         const subject = 'User Info';
-        const html = this.generateHtml(data);
-        await this.sendEmail(email, subject, html);
+        const htmlRecipient = this.generateHtmlForRecipient(data, subject);
+        const htmlReceiver = this.generateHtmlForReceiver(data);
+        await this.sendEmailToRecipient(email, subject, htmlRecipient);
+        await this.sendEmailToReceiver(subject, htmlReceiver);
     }
 
     async createBirth(email: string, data: CreateBirthDto): Promise<void> {
         const subject = 'Birth Info';
-        const html = this.generateHtml(data);
-        await this.sendEmail(email, subject, html);
+        const htmlRecipient = this.generateHtmlForRecipient(data, subject);
+        const htmlReceiver = this.generateHtmlForReceiver(data);
+        await this.sendEmailToRecipient(email, subject, htmlRecipient);
+        await this.sendEmailToReceiver(subject, htmlReceiver);
     }
 
     async createDeath(email: string, data: CreateDeathDto): Promise<void> {
         const subject = 'Death Info';
-        const html = this.generateHtml(data);
-        await this.sendEmail(email, subject, html);
+        const htmlRecipient = this.generateHtmlForRecipient(data, subject);
+        const htmlReceiver = this.generateHtmlForReceiver(data);
+        await this.sendEmailToRecipient(email, subject, htmlRecipient);
+        await this.sendEmailToReceiver(subject, htmlReceiver);
     }
 
     async createMarriage(email: string, data: CreateMarriageDto): Promise<void> {
         const subject = 'Marriage Info';
-        const html = this.generateHtml(data);
-        await this.sendEmail(email, subject, html);
+        const htmlRecipient = this.generateHtmlForRecipient(data, subject);
+        const htmlReceiver = this.generateHtmlForReceiver(data);
+        await this.sendEmailToRecipient(email, subject, htmlRecipient);
+        await this.sendEmailToReceiver(subject, htmlReceiver);
     }
 
     async createDivorce(email: string, data: CreateDivorceDto): Promise<void> {
         const subject = 'Divorce Info';
-        const html = this.generateHtml(data);
-        await this.sendEmail(email, subject, html);
+        const htmlRecipient = this.generateHtmlForRecipient(data, subject);
+        const htmlReceiver = this.generateHtmlForReceiver(data);
+        await this.sendEmailToRecipient(email, subject, htmlRecipient);
+        await this.sendEmailToReceiver(subject, htmlReceiver);
     }
 }
